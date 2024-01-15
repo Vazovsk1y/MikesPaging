ae982a291e08d5f19e51a6a9f60b2976983976c9
Vazovsk1y <anolrey.b@gmail.com>
2024-01-15 22:46:05 +0300

    -added tests project.

---
+++ tests/MikesPaging.UnitTests/Models/AllowedTestEntityNames.cs
@@ -0,0 +1,25 @@
+﻿namespace MikesPaging.UnitTests.Models;
+
+public static class AllowedTestEntityNames
+{
+    public static IEnumerable<string> All
+    {
+        get
+        {
+            return new List<IEnumerable<string>>
+            {
+                AllowedNamesForFirstName,
+                AllowedNamesForLastName,
+                AllowedNamesForCreated,
+                AllowedNamesForAge
+            }
+            .SelectMany(e => e)
+            .ToList();
+        }
+    }
+
+    public static readonly IReadOnlyCollection<string> AllowedNamesForFirstName = [nameof(TestEntity.FirstName), "first_name"];
+    public static readonly IReadOnlyCollection<string> AllowedNamesForLastName = [nameof(TestEntity.LastName), "last_name"];
+    public static readonly IReadOnlyCollection<string> AllowedNamesForCreated = [nameof(TestEntity.Created), "created_date"];
+    public static readonly IReadOnlyCollection<string> AllowedNamesForAge = [nameof(TestEntity.Age), "entity_age"];
+}
---
+++ tests/MikesPaging.UnitTests/Models/TestEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace MikesPaging.UnitTests.Models;
+
+internal class TestEntity
+{
+    public required Guid Id { get; init; }
+
+    public required string FirstName { get; init; }
+
+    public required string LastName { get; init; }
+
+    public required DateTimeOffset Created { get; init; }
+
+    public required uint Age { get; init; }
+}

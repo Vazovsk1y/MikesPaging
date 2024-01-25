1f697562865662e06c21db449fde24ca9485046b
Vazovsk1y <anolrey.b@gmail.com>
2024-01-25 20:05:45 +0300

    -added separate endpoints for sorting, paging and filtering in WebApi example project.

--- examples/MikesPaging.WebApi/Controllers/UsersController.cs
+++ examples/MikesPaging.WebApi/Controllers/UsersController.cs
@@ -63,4 +63,100 @@ public class UsersController(
 
         return Ok(new UsersPage(users, totalUsersCount, sortingOptions, filteringOptions, pagingOptions));
     }
+
+    [HttpPost("page")]
+    public IActionResult GetAsync(PagingOptionsModel pagingOptionsModel)
+    {
+        var pagingOptionsRes = pagingOptionsModel.ToOptions();
+        if (pagingOptionsRes.IsFailure)
+        {
+            return BadRequest(pagingOptionsRes.ErrorMessage);
+        }
+
+        var pagingOptions = pagingOptionsRes.Value;
+        int totalUsersCount = dbContext.Users.Count();
+
+        var result = dbContext
+            .Users
+            .Include(e => e.Accounts)
+            .OrderBy(e => e.FullName)
+            .AsNoTracking();
+
+        result = pagingManager.ApplyPaging(result, pagingOptions);
+
+        var users = result.Select(e => new UserDTO(
+            e.Id,
+            e.FullName,
+            e.Age,
+            e.Created,
+            e.Accounts.Select(i => new AccountDTO(i.Id, i.Followers)).ToList()
+            ))
+            .ToList();
+
+        return Ok(new UsersPage(users, totalUsersCount, null, null, pagingOptions));
+    }
+
+    [HttpPost("sort")]
+    public IActionResult GetAsync(SortingOptionsModel sortingOptionsModel)
+    {
+        var sortingOptionsRes = sortingOptionsModel.ToOptions<UsersSortingProperties>();
+        if (sortingOptionsRes.IsFailure)
+        {
+            return BadRequest(sortingOptionsRes.ErrorMessage);
+        }
+
+        var sortingOptions = sortingOptionsRes.Value;
+        int totalUsersCount = dbContext.Users.Count();
+
+        var result = dbContext
+            .Users
+            .Include(e => e.Accounts)
+            .OrderBy(e => e.FullName)
+            .AsNoTracking();
+
+        result = sortingManager.ApplySorting(result, sortingOptions);
+
+        var users = result.Select(e => new UserDTO(
+            e.Id,
+            e.FullName,
+            e.Age,
+            e.Created,
+            e.Accounts.Select(i => new AccountDTO(i.Id, i.Followers)).ToList()
+            ))
+            .ToList();
+
+        return Ok(new UsersPage(users, totalUsersCount, sortingOptions, null));
+    }
+
+    [HttpPost("filter")]
+    public IActionResult GetAsync(FilteringOptionsModel filteringOptionsModel)
+    {
+        var filteringOptionsRes = filteringOptionsModel.ToOptions<UsersFilteringProperties>();
+        if (filteringOptionsRes.IsFailure)
+        {
+            return BadRequest(filteringOptionsRes.ErrorMessage);
+        }
+
+        var filteringOptions = filteringOptionsRes.Value;
+        int totalUsersCount = filteringManager.ApplyFiltering(dbContext.Users, filteringOptions).Count();
+
+        var result = dbContext
+            .Users
+            .Include(e => e.Accounts)
+            .OrderBy(e => e.FullName)
+            .AsNoTracking();
+
+        result = filteringManager.ApplyFiltering(result, filteringOptions);
+
+        var users = result.Select(e => new UserDTO(
+            e.Id,
+            e.FullName,
+            e.Age,
+            e.Created,
+            e.Accounts.Select(i => new AccountDTO(i.Id, i.Followers)).ToList()
+            ))
+            .ToList();
+
+        return Ok(new UsersPage(users, totalUsersCount, null, filteringOptions, null));
+    }
 }
